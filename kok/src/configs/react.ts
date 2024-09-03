af8983f9b30d0e2440d21b382851f27132be6bbc
isqua <isqua@isqua.ru>
2024-09-04 02:57:00 +0300

    add more sources to the react digest

--- kok/src/configs/react.ts
+++ kok/src/configs/react.ts
@@ -4,8 +4,15 @@ import type { DigestConfig } from "../types/digest";
 const sources: string[] = [
     "https://github.com/facebook/react/releases.atom",
     "https://overreacted.io/rss.xml",
+    "https://github.com/reduxjs/redux/releases.atom",
+    "https://github.com/reduxjs/redux-toolkit/releases.atom",
+    "https://github.com/vitejs/vite/releases.atom",
+    "https://github.com/vitest-dev/vitest/releases.atom",
+    "https://github.com/testing-library/react-testing-library/releases.atom",
+    "https://github.com/testing-library/dom-testing-library/releases.atom",
     "https://devblogs.microsoft.com/typescript/feed/",
     "https://github.com/microsoft/TypeScript/releases.atom",
+    "https://github.com/nodejs/node/releases.atom",
 ];
 
 export const reactWeekly: DigestConfig = {
